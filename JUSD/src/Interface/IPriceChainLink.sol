e830f2e120df39ad93f326fad0feb3732ec29bd2
frimoldi.eth <francisco.rimoldi@gmail.com>
2023-04-17 12:27:43 -0300

    Delete JUSD directory

--- JUSD/src/Interface/IPriceChainLink.sol
+++
@@ -1,9 +0,0 @@
-/*
-    Copyright 2022 JOJO Exchange
-    SPDX-License-Identifier: BUSL-1.1*/
-pragma solidity 0.8.9;
-
-interface IPriceChainLink {
-    //    get token address price
-    function getAssetPrice() external view returns (uint256);
-}
