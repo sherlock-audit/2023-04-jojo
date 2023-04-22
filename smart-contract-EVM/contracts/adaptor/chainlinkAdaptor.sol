dec2a6a3af2f1e9d04e2378e51f4c1e76eb54282
sherlock-admin <dev@sherlock.xyz>
2023-04-22 19:11:37 +0000

    Copy smart-contract-EVM @ a2c104051a6ea4d1b6696bbe786c9e9cccd05df8

---
+++ smart-contract-EVM/contracts/adaptor/chainlinkAdaptor.sol
@@ -0,0 +1,56 @@
+/*
+    Copyright 2022 JOJO Exchange
+    SPDX-License-Identifier: BUSL-1.1
+*/
+
+pragma solidity 0.8.9;
+
+import "@openzeppelin/contracts/utils/math/SafeCast.sol";
+
+interface IChainlink {
+    function latestRoundData()
+        external
+        view
+        returns (
+            uint80 roundId,
+            int256 answer,
+            uint256 startedAt,
+            uint256 updatedAt,
+            uint80 answeredInRound
+        );
+
+    function latestAnswer() external view returns (int256 answer);
+}
+
+contract ChainlinkExpandAdaptor {
+    address public immutable chainlink;
+    uint256 public immutable decimalsCorrection;
+    uint256 public immutable heartbeatInterval;
+    address public immutable USDCSource;
+
+    constructor(
+        address _chainlink,
+        uint256 _decimalsCorrection,
+        uint256 _heartbeatInterval,
+        address _USDCSource
+    ) {
+        chainlink = _chainlink;
+        decimalsCorrection = 10**_decimalsCorrection;
+        heartbeatInterval = _heartbeatInterval;
+        USDCSource = _USDCSource;
+    }
+
+    function getMarkPrice() external view returns (uint256 price) {
+        int256 rawPrice;
+        uint256 updatedAt;
+        (, rawPrice, , updatedAt, ) = IChainlink(chainlink).latestRoundData();
+        (, int256 USDCPrice,, uint256 USDCUpdatedAt,) = IChainlink(USDCSource).latestRoundData();
+        require(
+            block.timestamp - updatedAt <= heartbeatInterval,
+            "ORACLE_HEARTBEAT_FAILED"
+        );
+        require(block.timestamp - USDCUpdatedAt <= heartbeatInterval, "USDC_ORACLE_HEARTBEAT_FAILED");
+        uint256 tokenPrice = (SafeCast.toUint256(rawPrice) * 1e8) / SafeCast.toUint256(USDCPrice);
+        return tokenPrice * 1e18 / decimalsCorrection;
+    }
+}
