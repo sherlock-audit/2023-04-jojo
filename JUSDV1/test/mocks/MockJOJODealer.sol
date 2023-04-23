e8366cd175550128b85756f90be9b5a227aaae46
frimoldi.eth <francisco.rimoldi@gmail.com>
2023-04-23 15:40:00 -0300

    Delete JUSDV1 directory

--- JUSDV1/test/mocks/MockJOJODealer.sol
+++
@@ -1,6 +0,0 @@
-// SPDX-License-Identifier: Unlicense
-pragma solidity 0.8.9;
-
-contract MockJOJODealer {
-    function deposit(uint256 primaryAmount, uint256 secondaryAmount, address to) external { }
-}
--- JUSDV1/test/mocks/MockJOJODealerRevert.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: Unlicense
-pragma solidity 0.8.9;
-
-contract MockJOJODealerRevert {
-    function deposit(uint256 primaryAmount, uint256 secondaryAmount, address to) external {
-        require(false, "test For revert");
-    }
-}
