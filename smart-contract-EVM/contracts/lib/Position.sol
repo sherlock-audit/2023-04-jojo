82763237375f7dd237a8f17f200df43aa1e18192
sherlock-admin <dev@sherlock.xyz>
2023-04-23 19:44:48 +0000

    Copy smart-contract-EVM @ 4a95a8e9a6367ae88dc827e29467229cb5bbad4f

---
+++ smart-contract-EVM/contracts/lib/Position.sol
@@ -0,0 +1,42 @@
+/*
+    Copyright 2022 JOJO Exchange
+    SPDX-License-Identifier: BUSL-1.1
+*/
+
+pragma solidity 0.8.9;
+
+import "../utils/Errors.sol";
+import "./Types.sol";
+
+library Position {
+    // ========== position register ==========
+
+    /// @notice add position when trade or liquidation happen
+    /// msg.sender is the perpetual contract
+    function _openPosition(Types.State storage state, address trader) internal {
+        state.openPositions[trader].push(msg.sender);
+    }
+
+    /// @notice realize pnl and remove position from the registry
+    /// msg.sender is the perpetual contract
+    function _realizePnl(
+        Types.State storage state,
+        address trader,
+        int256 pnl
+    ) internal {
+        state.primaryCredit[trader] += pnl;
+        state.positionSerialNum[trader][msg.sender] += 1;
+
+        address[] storage positionList = state.openPositions[trader];
+        for (uint256 i = 0; i < positionList.length;) {
+            if (positionList[i] == msg.sender) {
+                positionList[i] = positionList[positionList.length - 1];
+                positionList.pop();
+                break;
+            }
+            unchecked {
+                ++i;
+            }
+        }
+    }
+}
